f4a5f4140175aa8b201b16e8cc1d39f1b83204e1
Will Suttner <willsuttner@yahoo.com>
2021-10-07 23:31:43 -0700

    centralized validation wtih express-validator and improved form validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { body, validationResult} = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+
 
 const router = express.Router();
 
@@ -9,21 +12,42 @@ router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) =>{
-    const { email, password, passwordConfirmation } = req.body;
+router.post(
+    '/signup', 
+    body('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .custom(async (email) => {
+            const existingUser = await usersRepo.getOneBy({ email });
+            if (existingUser) {
+                throw new Error('Email in use');
+            }
+        }),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20})
+        .withMessage('Must be between 4 and 20 characters'),
+    body('passwordConfirmation')
+        .trim()
+        .isLength({ min: 4, max: 20})
+        .withMessage('Must be between 4 and 20 characters')
+        .custom((passwordConfirmation, { req }) => {
+            if (passwordConfirmation !== req.body.password) {
+                throw new Error('Passwords must match');
+            }
+        }), 
+async (req, res) =>{
+    const errors = validationResult(req);
+    console.log(errors);
 
-    const existingUser = await usersRepo.getOneBy({ email });
-    if(existingUser) {
-        return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Password must match');
-    }
+    const { email, password, passwordConfirmation } = req.body;    
 
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
+    
     res.send('Account Created!');
 });
 
