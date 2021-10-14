f8d877221e6eb79b4649144587f4842abd9a7b63
Will Suttner <willsuttner@yahoo.com>
2021-10-14 13:35:37 -0700

    finalized cart functionality

--- routes/carts.js
+++ routes/carts.js
@@ -34,7 +34,7 @@ router.post('/cart/products', async (req, res) => {
     // Either increment quantity for existing product
     // Or add new product to items array
 
-    res.send('Product added to cart');
+    res.redirect('/cart');
 });
 
 //Receive a get request to show all items in cart
@@ -55,5 +55,15 @@ router.get('/cart', async (req, res) => {
 });
 
 //Receive a post request to delete an item from a cart
+router.post('/cart/products/delete', async (req, res) => {
+    const { itemId } = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter(item => item.id !== itemId);
+
+    await cartsRepo.update(req.session.cartId, { items });
+
+    res.redirect('/cart');
+});
 
 module.exports = router;
